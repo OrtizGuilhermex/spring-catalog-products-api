1d28e38c7da96427c91e965b120ccd37b2222b7f
luiz_gf_ortiz <luiz_gf_ortiz@estudante.sesisenai.org.br>
2026-03-23 16:54:15 -0300

    feat: adiciona estrutura inical e dependencias

---
+++ src/main/java/com/ctw/spring_catalog_products_api/SpringCatalogProductsApiApplication.java
@@ -0,0 +1,13 @@
+package com.ctw.spring_catalog_products_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCatalogProductsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCatalogProductsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ctw/spring_catalog_products_api/SpringCatalogProductsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ctw.spring_catalog_products_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCatalogProductsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
